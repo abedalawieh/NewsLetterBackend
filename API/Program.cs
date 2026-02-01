ce1d4e4f7a75ff083bab0437c23d911e323cf5c1
Abed Alawieh <abed.alawiyeh@intalio.com>
2026-02-01 21:28:20 +0200

    Modified the cors

--- API/Program.cs
+++ API/Program.cs
@@ -85,14 +85,19 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.SetIsOriginAllowed(origin => 
-              {
-                  var uri = new Uri(origin);
-                  return uri.Host == "localhost" || uri.Host == "127.0.0.1";
-              })
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+        var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? Array.Empty<string>();
+        if (allowedOrigins.Length > 0)
+        {
+            policy.WithOrigins(allowedOrigins)
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
+        else
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
     });
 });
 
